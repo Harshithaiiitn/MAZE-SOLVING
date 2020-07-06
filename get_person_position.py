aff90f3c886363890ca3f459fa9f8225316c5aae
Siva Naga Harshitha B <Harshitha.b@ghc.local>
2020-07-06 15:37:55 +0530

    Added unit test for get person position

---
+++ get_person_position.py
@@ -0,0 +1,23 @@
+
+def test_get_person_position():
+    assert get_person_position('b', [['F', 'X', 'F', 'F', 'F', 'X'],
+                            ['g1', 'F', 'F', 'F', 'F', 'e'],
+                            ['F', 'b', 'F', 'F', 'F', 'F'],
+                            ['X', 'F', 'g2', 'F', 'F', 'X'],
+                            ['F', 'X', 'F', 'F', 'F', 'F'],
+                            ['F', 'F', 'F', 'F', 'F', 'F']]) == [2, 1],"Not showing exact position in the board"
+
+
+def get_person_position(person,board):
+    for row in range(0,len(board)):
+        for col in range(0,len(board)):
+            if board[row][col]==person:
+                position_row,position_column=row,col
+    positions=[position_row,position_column]
+    return positions
+
+
+    
+if __name__=="__main__":
+
+    test_get_person_position()
